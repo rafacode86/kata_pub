b63e69e5da036e5173c454d1425eac3fa996cb16
Rafa Bello <rafabp986@outlook.com>
2025-10-23 12:14:49 +0200

    init program, construct table

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/*Kata 64 per l'especialitat fullstackPHP 18-9-25
+
+Un bar de copes té taules pels clients:
+
+Cada taula té:
+
+Un nombre identificador
+Núm. de cadires
+Un indicador de si ja està reservada o no.
+Imagina't que ets un client/a que vol fer una reserva en aquest bar. Per fer-ho, haurem d'indicar:
+
+Nom de la reserva
+Quantitat de persones.
+Crea un programa que representi tota aquesta informació i lògiques proposades */
+
